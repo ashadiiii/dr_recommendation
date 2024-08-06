41106e4cc03a83fc6827f2567b9f5d7ebc1ac243
Ashadi De Silva <methasa@gmail.com>
2024-08-06 09:43:33 +0100

    new changes

--- drug_recom.py
+++ drug_recom.py
@@ -68,27 +68,33 @@ x_train,x_test,y_train,y_test = train_test_split(x_feat,y,stratify=y,test_size=0
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+#creating a vectorizer object and converting the training and test data to tfidf values
 tfidf_vectorizer = TfidfVectorizer(stop_words='english',max_df=0.8,ngram_range=(1,3))
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
 tfidf_test = tfidf_vectorizer.transform(x_test)
 
+#creating a passive aggressive classifier object and training the model with the tfidf training data
 pass_tf = PassiveAggressiveClassifier()
 pass_tf.fit(tfidf_train,y_train)
+
+#evaluating the performance of the model with the tfidf test data
 pred = pass_tf.predict(tfidf_test)
 score = metrics.accuracy_score(y_test,pred)
 print(f"accuracy: {score}")
 
+#experimenting the model with actual input data
 text = ["nexplanon since dec 27 201 got first period end january lasted month half march 201 bleed close three week started bleeding march 28th bleeding every since gained 1 lb far since getting birth control although weight gain deal breaker bleeding is. trying patient see body adjusts implant three month far finger crossed cycle go away awhile."]
 test = tfidf_vectorizer.transform(text)
 pred = pass_tf.predict(test)[0]
 print(pred)
 
+#creating the drug recommendation function
 def top_drugs_extractor(condition,df):
     df_top = df[(df['rating']>=9) & (df['usefulCount']>= 100)].sort_values(by =['rating','usefulCount'],ascending=[False,False])
     drug_lst = df_top[df_top['condition']== condition]['drugName'].head(3).tolist()
     return drug_lst
 
+#experimenting the function
 drugs = top_drugs_extractor('Depression',df)
 print(drugs)
 
